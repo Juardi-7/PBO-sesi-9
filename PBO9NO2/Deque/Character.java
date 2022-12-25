78090c3921659de2c074452b8a8d91cddedda914
Juardi-7 <114724288+Juardi-7@users.noreply.github.com>
2022-12-25 09:30:08 +0700

    Add files via upload

---
+++ PBO9NO2/Deque/Character.java
@@ -0,0 +1,9 @@
+package PBO9NO2.Deque;
+
+public abstract class Character {
+    abstract String getName();
+    abstract float getHp();
+    abstract float getAttack();
+    abstract float getDefense();
+    public abstract void infoCharacter();
+}
---
+++ PBO9NO2/Deque/Spesialist.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PBO9NO2.Deque;
+
+/**
+ *
+ * @author user
+ */
+public class Spesialist {
+    
+}
